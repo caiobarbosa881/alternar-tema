e0722388bee444349e218163362845b27dadce62
Caio Barbosa <caiobarbosasoriano@gmail.com>
2022-04-19 14:33:37 -0300

    first commit

---
+++ src/styles/themes/dark.ts
@@ -0,0 +1,11 @@
+export default {
+    title: 'dark',
+
+    colors: {
+        primary: '#A8D5E2',
+        secundary: '#F9A620',
+        
+        background: '#222',
+        text: '#fff'
+  },
+};
---
+++ src/styles/themes/light.ts
@@ -0,0 +1,11 @@
+export default {
+    title: 'light',
+
+    colors: {
+      primary: '#A8D5E2',
+      secundary: '#F9A620',
+        
+        background: '#f5f5f5',
+        text: '#333'
+  },
+};
